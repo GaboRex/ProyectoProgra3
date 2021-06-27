1caa5c450411080f85f8f6c4fd4f9b93ce9e0ffa
GaboRex <80688833+GaboRex@users.noreply.github.com>
2021-06-26 22:13:47 -0400

    Autenticacion-Avance Gabo

--- app/src/main/java/com/example/agrupacionmarilyn/LoginActivity.kt
+++ app/src/main/java/com/example/agrupacionmarilyn/LoginActivity.kt
@@ -3,32 +3,65 @@ package com.example.agrupacionmarilyn
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-    lateinit var buttonIniciarSesion: Button
-    lateinit var buttonRegistrarse: Button
-    lateinit var editTextUsuario: EditText
-    lateinit var editTextPassword: EditText
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        setup()
 
-        buttonIniciarSesion = findViewById(R.id.buttonIniciarSesion)
-        buttonRegistrarse = findViewById(R.id.buttonRegistrarse)
-        editTextUsuario = findViewById(R.id.editTextUsuario)
-        editTextPassword = findViewById(R.id.editTextPassword)
+    }
 
-        buttonIniciarSesion.setOnClickListener {
-            val intent = Intent(this, LocationActivity::class.java)
-            startActivity(intent)
-        }
-
+    private fun setup() {
+        title = "Autenticacion"
         buttonRegistrarse.setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
+            if (editTextUsuario.text.isNotEmpty() && editTextPassword.text.isNotEmpty()) {
+                FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(
+                        editTextUsuario.text.toString(), editTextPassword.text.toString()
+                    ).addOnCompleteListener {
+
+                        if (it.isSuccessful) {
+                            showLocation(it.result?.user?.email ?: "" )
+                        } else {
+                            showAlert()
+                        }
+                    }
+            }
         }
+        buttonIniciarSesion.setOnClickListener {
+            if (editTextUsuario.text.isNotEmpty() && editTextPassword.text.isNotEmpty())
+                FirebaseAuth.getInstance()
+                    .signInWithEmailAndPassword(
+                        editTextUsuario.text.toString(),editTextPassword.text.toString()
+                    ).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            showLocation(it.result?.user?.email ?: "")
+                        } else {
+                            showAlert()
+                        }
+                    }
+        }
+    }
+
+    private fun showAlert() {
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando el usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showLocation(email: String ) {
+        val intent = Intent(this, LocationActivity::class.java).apply {
+            putExtra("username", email)
+            putExtra("email", email)
+        }
+        startActivity(intent)
     }
 }
