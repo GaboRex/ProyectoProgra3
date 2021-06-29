c675be4d07c3eaf0eb7c7b415a40552f3550c28b
GaboRex <80688833+GaboRex@users.noreply.github.com>
2021-06-29 19:04:10 -0400

    Avance Gabo 29/06/21

---
+++ app/src/main/java/com/example/agrupacionmarilyn/MapActivity.kt
@@ -0,0 +1,24 @@
+package com.example.agrupacionmarilyn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        mMap = p0
+
+    }
+}
