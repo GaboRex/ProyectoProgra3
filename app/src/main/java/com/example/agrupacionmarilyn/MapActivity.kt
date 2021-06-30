55f05c93b78853712d1fc3e5f6827313bcb13045
GaboRex <80688833+GaboRex@users.noreply.github.com>
2021-06-30 01:12:31 -0400

    2do Avance Gabo 29/06/21

--- app/src/main/java/com/example/agrupacionmarilyn/MapActivity.kt
+++ app/src/main/java/com/example/agrupacionmarilyn/MapActivity.kt
@@ -1,10 +1,14 @@
 package com.example.agrupacionmarilyn
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
@@ -15,10 +19,17 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
     }
 
     override fun onMapReady(p0: GoogleMap) {
         mMap = p0
-
+        val Alexander = LatLng(-16.54075003806811, -68.07977942684812)
+        mMap.addMarker(
+            MarkerOptions()
+                .position(Alexander)
+                .title("Marker in Elis")
+        )
+        mMap.animateCamera(CameraUpdateFactory.zoomIn())
     }
 }
--- app/src/main/java/com/example/agrupacionmarilyn/ProductoLugar_Activity.kt
+++ app/src/main/java/com/example/agrupacionmarilyn/ProductoLugar_Activity.kt
@@ -2,10 +2,23 @@ package com.example.agrupacionmarilyn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 
 class ProductoLugar_Activity : AppCompatActivity() {
+    lateinit var imagen : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_producto_lugar)
+
+        imagen = findViewById(R.id.imagen1)
+
+        var url : String = "https://z-p3-scontent.flpb2-2.fna.fbcdn.net/v/t1.6435-9/101220345_107515010989216_2695433518334345216_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=WcoUj5LPUnwAX_N8iC2&tn=OdT12UAe4RLhgOYF&_nc_ht=z-p3-scontent.flpb2-2.fna&oh=4d3247f8ed3935c143020c5ef62d62d6&oe=60DBE3DF"
+        Glide.with(this)
+            .load(url)
+            .into(imagen)
     }
 }
